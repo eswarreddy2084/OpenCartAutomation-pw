8f056d0eac4b293ca0ca823a7fe2a33be0869160
Eswar reddy sangireddy <Eswarreddy9580@gmail.com>
2026-03-22 18:20:42 -0500

    updated config file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,43 +1,39 @@
 import { defineConfig, devices } from "@playwright/test";
+import "allure-playwright"; // ⭐ REQUIRED for Jenkins to generate allure-results
 
 export default defineConfig({
   timeout: 30 * 1000,
   testDir: "./tests",
-  /* Run tests in files in parallel */
+
   fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
   retries: 1,
-  /* Opt out of parallel tests on CI. */
   workers: 5,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+
   reporter: [
-    ["html"],
-    ["allure-playwright", { outputFolder: "allure-results" }],
     ["list"],
+    ["allure-playwright", { outputFolder: "allure-results" }], // ⭐ Correct folder
+    ["html"],
   ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
     trace: "on-first-retry",
-    screenshot: "on-first-failure",
+    screenshot: "only-on-failure",
     video: "retain-on-failure",
-    viewport: { width: 1280, height: 720 }, // setting default viewport size for consistency.
-    ignoreHTTPSErrors: true, // Ignore SSL errors if necessary
-    permissions: ["geolocation"], // Set necessary permissions for geolocation-based tests
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    permissions: ["geolocation"],
   },
-  // grep:/@master/
+
   projects: [
     {
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-
     {
       name: "firefox",
       use: { ...devices["Desktop Firefox"] },
     },
-
     {
       name: "webkit",
       use: { ...devices["Desktop Safari"] },
